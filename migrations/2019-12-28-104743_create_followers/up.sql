cb1ed7a3f6922a8eccade9450b226a8fcffdcade
wolf4ood <enrico.risa@gmail.com>
2022-04-28 23:04:18 +0200

    removed diesel migrations

--- migrations/2019-12-28-104743_create_followers/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE followers (
-   followed_id UUID NOT NULL,
-   follower_id UUID NOT NULL,
-   FOREIGN KEY (followed_id) REFERENCES users(id) ON DELETE CASCADE,
-   FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
-   PRIMARY KEY (followed_id, follower_id)
-);
